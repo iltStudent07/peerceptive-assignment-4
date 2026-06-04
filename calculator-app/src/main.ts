d0710e58dc9742ed3deebef0e06ce596b67c7202
iltStudent07 <iltStudent07@example.com>
2026-06-04 18:44:03 +0000

    Edit content in style.css and remove counter.ts

--- calculator-app/src/main.ts
+++ calculator-app/src/main.ts
@@ -2,7 +2,6 @@ import './style.css'
 import typescriptLogo from './assets/typescript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="center">
@@ -56,5 +55,3 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div class="ticks"></div>
 <section id="spacer"></section>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
