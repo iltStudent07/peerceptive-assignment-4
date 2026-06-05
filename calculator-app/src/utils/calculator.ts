366c7470e3c87a4e428f235e36669bef8d771f58
iltStudent07 <iltStudent07@example.com>
2026-06-05 15:32:00 +0000

    Add content to Calculator class

--- calculator-app/src/utils/calculator.ts
+++ calculator-app/src/utils/calculator.ts
@@ -12,5 +12,59 @@ export class Calculator {
         } else {
             this.currentInput += number;
         }
+        this.updateDisplay();
+    }
+
+    public chooseOperator(operator: Operator): void {
+        if (this.currentInput === '') return;
+        if (this.previousInput !== '') {
+            this.compute();
+        }
+        this.operator = operator;
+        this.previousInput = this.currentInput;
+        this.currentInput = '';
+    }
+
+    public compute(): void {
+        let computation: number;
+        const prev = parseFloat(this.previousInput);
+        const current = parseFloat(this.currentInput);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operator) {
+            case '+':
+                computation = prev + current;
+                break;
+            
+            case '-':
+                computation = prev + current;
+                break;
+
+            case '*':
+                computation = prev * current;
+                break;
+
+            case '/':
+                computation = prev / current;
+                break;
+            
+            default:
+                break;
+        }
+        this.currentInput = computation.toString();
+        this.operator = null;
+        this.previousInput = '';
+        this.updateDisplay();
+    }
+
+    public updateDisplay(): void {
+        const display = document.getElementById('display') as HTMLInputElement;
+        display.value = this.currentInput;
+    }
+
+    public clear(): void {
+        this.currentInput = '';
+        this.previousInput = '';
+        this.operator = null;
+        this.updateDisplay();
     }
 }
